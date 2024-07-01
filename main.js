cdbc2957808fa42d32afb54d38440b4c23cde2b0
gabrielcorsinha <129746910+gabrielcorsinha@users.noreply.github.com>
2024-07-01 08:53:46 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+const caixaPrincipal = document.querySelector("Caixa-principal");
+const caixaPerguntas = document.querySelector("Caixa-perguntas");
+const caixaAlternativas = document.querySelector("Caixa-alternativas");
+const caixaResultado = document.querySelector("Caixa-resultado");
+
+const perguntas = [
+    {
+    enunciado: "pergunta 1",
+    alternativas: ["alternativa 1",
+        "alternativa 2"]
+    },
+
+    {
+    enunciado: "pergunta2",
+        akternativas: ["alternativa 1",
+            "alternativa 2"]
+    },
+]
+
+
